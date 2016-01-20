d5be0226c4d3be7ffd209af6c6dc113b4b247494
henugao <252691665@qq.com>
2016-01-20 09:35:20 +0800

    处理下拉时候的删除和点击操作

--- src/com/henugao/pullselect/MainActivity.java
+++ src/com/henugao/pullselect/MainActivity.java
@@ -3,12 +3,18 @@ package com.henugao.pullselect;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.crypto.spec.IvParameterSpec;
+
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -21,8 +27,9 @@ public class MainActivity extends Activity {
 	private EditText et_content;
 	private ImageView iv_pull;
 	private ListView listView;
-	private int popWindowHeigth = 500;
+	private int popWindowHeigth = 300;
 	private List<String> list = new ArrayList<String>();
+	private PopupWindow popupWindow;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,8 +43,20 @@ public class MainActivity extends Activity {
 			list.add("100000"+i);
 		}
 		listView = new ListView(MainActivity.this);
+		listView.setVerticalScrollBarEnabled(false); //隐藏垂直方向的listview的滚动条
 		MyAdapter adapter = new MyAdapter();
 		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Log.i("MainActivity", "posi:"+position);
+				et_content.setText(list.get(position));
+				
+			}
+		});
+		
 	}
 
 	private void initUi() {
@@ -55,8 +74,17 @@ public class MainActivity extends Activity {
 	}
 	
 	private void showPopWindow() {
-		PopupWindow popupWindow = new PopupWindow(listView,et_content.getWidth(), popWindowHeigth);
+		if (popupWindow == null) {
+			popupWindow = new PopupWindow(listView,et_content.getWidth(), popWindowHeigth);
+		}
+
+		popupWindow.setFocusable(true);
+		popupWindow.setBackgroundDrawable(new BitmapDrawable());
+		//popupWindow.setOutsideTouchable(true);
+		//popupWindow.setTouchable(true);
 		popupWindow.showAsDropDown(et_content, 0, 0);
+
+		
 	}
 	class MyAdapter extends BaseAdapter {
 
@@ -79,12 +107,26 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = View.inflate(MainActivity.this, R.layout.adapter_list, null);
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			final View view = View.inflate(MainActivity.this, R.layout.adapter_list, null);
 			TextView tvUser = (TextView) view.findViewById(R.id.tv_user);
 			ImageView ivDel = (ImageView) view.findViewById(R.id.iv_del);
 			tvUser.setText(list.get(position));
-			
+			ivDel.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					list.remove(position);
+					notifyDataSetChanged();
+					
+					int listviewHeight = view.getHeight() * list.size();
+					popupWindow.update(et_content.getWidth(), listviewHeight > popWindowHeigth ? popWindowHeigth : listviewHeight);
+					if (list.size() == 0) {
+						popupWindow.dismiss();
+						iv_pull.setVisibility(View.INVISIBLE);
+					}
+				}
+			});
 			return view;
 		}
 		
